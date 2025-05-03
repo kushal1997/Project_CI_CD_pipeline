fb909828b2e34ab3dd32b303c36b41a491f20518
kushal1997 <kushalrao103@gmail.com>
2025-05-03 04:30:59 +0000

    WIP : renamed & added a feature to make a backup folder

--- after_run.sh
+++ after_run.sh
@@ -1,21 +1,31 @@
 #!/bin/bash
 
+sudo apt install nginx
+
 sudo systemctl stop nginx
 
-[ ! -f index.html ] && sudo touch index.html
+#[ ! -f index.html ] && sudo touch index.html
 
 # [ -f index.html ] && echo " =========== index file is there=========="
 # [ ! -f index.html ] && echo " =========== index file is not there=========="
 
-sudo cp index.html /var/www/html/index.html
+# count=$(ls /var/www/html/ | grep -i 'index.html' | wc -l)
+
+#if [ $count -eq 0 ]; then
+#	sudo touch  /var/www/html/index.html
+#fi
+
+sudo cp index.html /var/www/html/
+
+[ ! -d /etc/nginx/sites-available/default_backup ] && sudo mkdir /etc/nginx/sites-available/default_backup
 
 timestamp=$(date +"%Y-%m-%d_%H-%M")
 
-sudo cp /etc/nginx/sites-available/default /etc/nginx/sites-available/default_${timestamp}.old
+sudo cp /etc/nginx/sites-available/default /etc/nginx/sites-available/default_backup/default_${timestamp}.old
 
 sudo cp sample_default_nginix_config /etc/nginx/sites-available/default
 
-sudo apt install nginx
+sudo nginx -t
 
 sudo systemctl reload nginx
 
