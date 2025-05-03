761305d02a9bb7dafb5d9fc66fbac24160980ca6
kushal1997 <kushalrao103@gmail.com>
2025-05-03 06:48:43 +0000

    WIP 'BIGN': added a check for branch name

--- beginner/run.sh
+++ beginner/run.sh
@@ -1,5 +1,29 @@
-git pull origin
+#!/bin/bash
 
-chmod +x after_run.sh
+curr_branch=$(git branch --show-current)
 
-sh after_run.sh
+if [[ "$curr_branch" == "dev" ]]; then
+
+	if git pull origin dev; then
+		echo "===================== Successful PULL request ======================="
+
+		chmod +x after_run.sh
+
+		sh after_run.sh
+	else
+		echo "================ ERROR: Pull request is unsuccessful ========================"
+	fi
+else
+	git checkout dev
+
+	if git pull origin dev; then
+                 echo "===================== Successful PULL request ======================="
+
+                 chmod +x after_run.sh
+
+                 sh after_run.sh
+         else
+                 echo "================ ERROR: Pull request is unsuccessful ========================"
+         fi
+
+fi
